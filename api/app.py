791aeeeacb58eaed1cb7eb7c00ff3ec693f73c4b
MinCarvalho <iyasmin.carvalho99@hotmail.com>
2023-12-03 15:25:35 -0300

    db

--- api/app.py
+++ api/app.py
@@ -6,6 +6,8 @@ from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ljjr1989jjlyyaf'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.abspath('checklist.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # Exibir mensagens detalhadas no SQLAlchemy
+app.config['DEBUG'] = True  # Ativar o modo de depuração
 
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
